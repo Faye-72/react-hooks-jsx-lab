e1b6848f8dbde33741d35191f79cf486621ad478
fmitei <faith.mitei@trustgro.com>
2023-01-31 17:51:56 +0300

    rectified styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import { name, city } from "../data/data.js";
 function Home() {
   // update the JSX being returned!
   return (
-    <div id="home">
+    <div id="home" style={{ color: "firebrick" }}>
       <h1>
         {name} is a Web Developer from {city}
       </h1>
